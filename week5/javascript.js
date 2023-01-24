3fea33771bbae7cd9a08a48d2d729f5825f4f99f
John Rey <jmnazareno@student.apc.edu.ph>
2023-01-24 19:38:21 +0800

    overall update

--- week5/javascript.js
+++ week5/javascript.js
@@ -1,16 +1,7 @@
-const d = new Date();
-document.getElementById("date").innerHTML = d;
-console.log('Hello World');
-
-function myFunction() {
-    document.getElementById("button").innerHTML = "Hello World";
-  }
-
-function myFunction1() {
-    alert("Hello! I am an alert box!");
-  }
-
-function myFunction2() {
-      var myWindow = window.open("", "", "width=200, height=100");
-      myWindow.blur();
-    }
+const links = document.querySelectorAll(".scroll_to");
+links.forEach((item)=>{
+    item.addEventListener("click", ()=>{
+        const el = document.getElementById(item.getAttribute("data-link"));
+        el.scrollIntoView({behavior:"smooth", block:"end"})
+    })
+})
